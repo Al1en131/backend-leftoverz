6a513fb610de93d654f713039a1e8aea61d96067
Alif Essa Nurcahyani <essanurcahyania@gmail.com>
2025-04-28 19:06:19 +0800

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -61,6 +61,7 @@ const login = async (req, res) => {
     res.json({
       message: "Login successful",
       token: token,
+      role: user.role,
     });
   } catch (error) {
     console.error(error);
