3a084ea31fd2dc5d9d6ae7c4582a0f7e6728a825
Alif Essa Nurcahyani <essanurcahyania@gmail.com>
2025-04-29 22:15:17 +0700

    update api

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,24 +5,58 @@ const getAllUsers = async (req, res) => {
     const users = await User.findAll();
     if (!users || users.length === 0) {
       return res.status(404).json({
-        message: "No users found"
+        message: "No users found",
       });
     }
-    const usersData = users.map(user => user.toJSON());
+    const usersData = users.map((user) => user.toJSON());
 
     return res.status(200).json({
       message: "Users retrieved successfully",
-      users: usersData
+      users: usersData,
     });
   } catch (error) {
     console.error("Error fetching users:", error.message);
     return res.status(500).json({
       message: "An error occurred while retrieving users",
-      error: error.message
+      error: error.message,
+    });
+  }
+};
+
+const getUserById = async (req, res) => {
+  const userId = req.params.id;
+
+  if (!userId) {
+    return res.status(400).json({
+      message: "User ID is required",
+    });
+  }
+
+  try {
+    // Cari pengguna berdasarkan ID
+    const user = await User.findOne({ where: { id: userId } });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    // Mengubah data pengguna menjadi format JSON dan mengirimkan respons
+    return res.status(200).json({
+      message: "User retrieved successfully",
+      user: user.toJSON(),
+    });
+  } catch (error) {
+    console.error("Error fetching user:", error.message);
+    return res.status(500).json({
+      message: "An error occurred while retrieving the user",
+      error: error.message,
     });
   }
 };
 
 module.exports = {
-  getAllUsers
+  getAllUsers,
+  getUserById,
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,10 +3,12 @@ const authMiddleware = require("../middlewares/authMiddleware");
 
 const routes = express.Router();
 const { register, login } = require("../controllers/authController");
-const { getAllUsers } = require("../controllers/userController");
+const { getAllUsers, getUserById } = require("../controllers/userController");
+const { route } = require(".");
 
 routes.post("/login", login);
 routes.post("/register", register);
 routes.get("/users", authMiddleware, getAllUsers);
+routes.get("/user/:id", getUserById)
 
 module.exports = routes;
