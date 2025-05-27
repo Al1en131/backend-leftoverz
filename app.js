7fdb7fb391e63cc7ed13d3cda38c38384adb324b
Alif Essa Nurcahyani <essanurcahyania@gmail.com>
2025-05-27 19:55:46 +0700

    update api

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const app = express();
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(routes);
 
