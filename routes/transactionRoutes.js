fe760e06bc6c5ee49b5725b9258f713fd779d19f
Alif Essa Nurcahyani <essanurcahyania@gmail.com>
2025-06-10 23:36:08 +0700

    update

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -2,12 +2,13 @@ const express = require("express");
 const authMiddleware = require("../middlewares/authMiddleware"); 
 
 const routes = express.Router();
-const { getAllTransactions, getTransactionsByUserId, createMidtransToken, saveTransaction, handleMidtransWebhook, getTransactionByUserIdById, editTransactionByUserId } = require("../controllers/transactionController");
+const { getAllTransactions, getTransactionsByUserId, createMidtransToken, saveTransaction, handleMidtransWebhook, getTransactionByUserIdById, editTransactionByUserId, refundTransaction } = require("../controllers/transactionController");
 
 routes.get('/transactions/user/:userId', getTransactionsByUserId);
 routes.get("/transactions", authMiddleware, getAllTransactions);
 routes.post("/create-midtrans-token",  authMiddleware, createMidtransToken);
 routes.post("/create/transaction",  authMiddleware, saveTransaction);
+routes.post("/:order_id/refund", refundTransaction);
 routes.post('/midtrans/webhook',handleMidtransWebhook);
 routes.get("/:userId/transaction/:transactionId", getTransactionByUserIdById);
 routes.put("/transactions/:id", editTransactionByUserId);
